00109c6427bb0af2460d79eb6da918f1f4f4f6ac
Craxy <Craxyss+GitHub@gmail.com>
2015-03-29 22:21:18 +0200

    Updated version to 0.3.1

--- src/ToggleTrafficLights/Properties/AssemblyInfo.cs
+++ src/ToggleTrafficLights/Properties/AssemblyInfo.cs
@@ -33,9 +33,9 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 #if DEBUG
-[assembly: AssemblyVersion("0.3.0.*")]
-[assembly: AssemblyFileVersion("0.3.0.*")]
+[assembly: AssemblyVersion("0.3.1.*")]
+[assembly: AssemblyFileVersion("0.3.1.*")]
 #else
-[assembly: AssemblyVersion("0.3.0")]
-[assembly: AssemblyFileVersion("0.3.0")]
+[assembly: AssemblyVersion("0.3.1")]
+[assembly: AssemblyFileVersion("0.3.1")]
 #endif
