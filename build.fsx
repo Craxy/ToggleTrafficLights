c55b0eff99450e55ec715d7a13363b9c2892db19
Craxy <Craxyss+GitHub@gmail.com>
2015-04-13 03:03:03 +0200

    Started: build&upload to github via fake

---
+++ build.fsx
@@ -0,0 +1,14 @@
+// --------------------------------------------------------------------------------------
+// FAKE build script
+// --------------------------------------------------------------------------------------
+
+#r @"packages/FAKE/tools/FakeLib.dll"
+
+open Fake
+open Fake.Git
+open Fake.AssemblyInfoFile
+open Fake.ReleaseNotesHelper
+open System
+open System.IO
+
+
